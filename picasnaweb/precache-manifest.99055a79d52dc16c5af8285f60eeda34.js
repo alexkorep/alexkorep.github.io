ac3915181734de49a863b67bc8f3ed1a1bc17a5b
Alexey Korepanov <alexkorep@gmail.com>
2019-06-16 21:52:23 +0200

    Add picasnaweb

---
+++ picasnaweb/precache-manifest.99055a79d52dc16c5af8285f60eeda34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866cd74e1da538414c7a2dfd0ef346dc",
+    "url": "/picasnaweb/index.html"
+  },
+  {
+    "revision": "f99c489e975d3b1dad23",
+    "url": "/picasnaweb/static/css/main.8e494e79.chunk.css"
+  },
+  {
+    "revision": "c206781ada65be6a8942",
+    "url": "/picasnaweb/static/js/2.136ec0fb.chunk.js"
+  },
+  {
+    "revision": "f99c489e975d3b1dad23",
+    "url": "/picasnaweb/static/js/main.9634fb3e.chunk.js"
+  },
+  {
+    "revision": "c752291c7577ea5abe14",
+    "url": "/picasnaweb/static/js/runtime~main.59e03257.js"
+  }
+]);
